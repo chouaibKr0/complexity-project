0bf9da4cffca998e1c10cf4f8e1cf095117478f3
chouaibKr0 <karballac@gmail.com>
2025-12-26 17:24:42 +0100

    add file for SAT -> Sub sum

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,12 @@
 """NP-Complexity Project - Main Package."""
 from .solvers import SATSolver, ThreeSATSolver, SubsetSumSolver
 from .verifiers import verify_sat_solution, verify_3sat_solution, verify_subset_sum_solution
-from .reductions import reduce_sat_to_3sat, reduce_3sat_to_subset_sum
+from .reductions import reduce_sat_to_3sat, reduce_3sat_to_subset_sum, reduce_sat_to_subset_sum
 
 __version__ = "0.1.0"
 
 __all__ = [
     "SATSolver", "ThreeSATSolver", "SubsetSumSolver",
     "verify_sat_solution", "verify_3sat_solution", "verify_subset_sum_solution",
-    "reduce_sat_to_3sat", "reduce_3sat_to_subset_sum",
+    "reduce_sat_to_3sat", "reduce_3sat_to_subset_sum", "reduce_sat_to_subset_sum"
 ]
