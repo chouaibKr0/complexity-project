830a9eee40ec9ebdcf2a8926aa958da1568ff76a
chouaibKr0 <karballac@gmail.com>
2025-12-30 10:13:12 +0100

    add verify_sar_solver_result

--- src/verifiers/__init__.py
+++ src/verifiers/__init__.py
@@ -1,10 +1,12 @@
 """Verifiers package."""
-from .sat_verifier import verify_sat_solution
-from .three_sat_verifier import verify_3sat_solution
+from .sat_verifier import verify_sat_solution, verify_sat_solver_result
+from .three_sat_verifier import verify_3sat_solution, verify_3sat_solver_result
 from .subset_sum_verifier import verify_subset_sum_solution
 
 __all__ = [
     "verify_sat_solution",
     "verify_3sat_solution", 
     "verify_subset_sum_solution",
+    "verify_sat_solver_result",
+    "verify_3sat_solver_result",
 ]
